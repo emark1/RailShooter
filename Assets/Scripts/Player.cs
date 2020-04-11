ef46094b85f7982f18c5befee82914eeed61af3d
emark1 <devel010@umn.edu>
2020-04-10 19:54:53 -0500

    Ship on camera

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float xSpeed = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        transform.localPosition = new Vector3(rawNewXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+}
