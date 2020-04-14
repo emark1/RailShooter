86c0260b3acc9ba24bb27850dd69019614af64a8
emark1 <devel010@umn.edu>
2020-04-13 21:37:00 -0500

    Pitch

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,9 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     [SerializeField] float xSpeed = 4f;
+    [SerializeField] float ySpeed = 4f;
+    [SerializeField] float positionPitchFactor = .16f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +18,29 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation() {
+        float pitch = transform.localPosition.y * positionPitchFactor;
+        float yaw = 0f;
+        float roll = 0f;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation () {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
         float xOffset = xThrow * xSpeed * Time.deltaTime;
-        float rawNewXPos = transform.localPosition.x + xOffset;
-        transform.localPosition = new Vector3(rawNewXPos, transform.localPosition.y, transform.localPosition.z);
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+
+        float rawNewXPos = Mathf.Clamp(transform.localPosition.x + xOffset, -18.5f, 18.5f);
+        float rawNewYPos = Mathf.Clamp(transform.localPosition.y + yOffset, -14f, 14.8f);
+
+
+        transform.localPosition = new Vector3(rawNewXPos, rawNewYPos, transform.localPosition.z);
     }
 }
